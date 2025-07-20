40234ddf8161056a4ffc304e14eb0c2d2002270b
Jose Cabrera <jcabrera9409@gmail.com>
2025-07-20 06:32:31 +0000

    fixed dockerfile

--- zekret-front/src/environments/environment.ts
+++ zekret-front/src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
-    apiUrl: '',
+    apiUrl: 'http://localhost/v1',
     token_name: 'access_token',
-    domains: [],
-    disallowedRoutes: []
+    domains: ['localhost'],
+    disallowedRoutes: ['http://localhost/v1/auth/login', 'http://localhost/v1/users/register']
 };
