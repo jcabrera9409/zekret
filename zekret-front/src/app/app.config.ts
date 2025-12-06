c9d5c337b862fc19c763447a5f29900c749dcb63
Jose Cabrera <jcabrera9409@gmail.com>
2025-12-06 12:11:28 -0500

    Replace angular-jwt library with custom auth interceptor

--- zekret-front/src/app/app.config.ts
+++ zekret-front/src/app/app.config.ts
@@ -1,35 +1,44 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, inject } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
-import { JwtModule } from '@auth0/angular-jwt';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideHttpClient, withInterceptors, withInterceptorsFromDi } from '@angular/common/http';
+import { HttpInterceptorFn, provideHttpClient, withInterceptors } from '@angular/common/http';
 import { errorInterceptor } from './interceptors/error.interceptor';
+import { UtilMethods } from './util/util';
 
-// Token getter function para JWT usando sessionStorage directamente
-export function jwtTokenGetter(): string {
-  return sessionStorage.getItem('access_token') || '';
-}
+const authInterceptor: HttpInterceptorFn = (req, next) => {
+  const utilMethods = inject(UtilMethods);
+  const url = req.url;  
+  // URLs que NO deben tener Authorization header
+  const authFreeRoutes = [
+    '/v1/auth/login',
+    '/v1/users/register'
+  ];
+  
+  const shouldSkipAuth = authFreeRoutes.some(route => url.includes(route));
+  
+  if (shouldSkipAuth) {
+    return next(req);
+  }
+  
+  const token = utilMethods.getJwtToken();
+  if (token) {
+    req = req.clone({
+      setHeaders: {
+        Authorization: `Bearer ${token}`
+      }
+    });
+  }
+  
+  return next(req);
+};
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    importProvidersFrom(
-      JwtModule.forRoot({
-        config: {
-          tokenGetter: jwtTokenGetter,
-          allowedDomains: ['localhost:8080'],
-          disallowedRoutes: [
-            'http://localhost:8080/v1/auth/login',
-            'http://localhost:8080/v1/users/register'
-          ]
-        }
-      })
-    ),
+    UtilMethods,
     provideHttpClient(
-      withInterceptorsFromDi(),
-      withInterceptors([errorInterceptor])
+      withInterceptors([authInterceptor, errorInterceptor])
     ),
     provideRouter(routes), 
     provideAnimationsAsync()
